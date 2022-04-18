6b486fd8bca05681f8affa152f81e11609c6a952
Necati <necatisenel@marun.edu.tr>
2022-04-18 17:41:18 +0300

    student login function.

---
+++ src/com/jean/DatabaseManager.java
@@ -0,0 +1,57 @@
+package com.jean;
+import java.sql.*;
+
+public class DatabaseManager {
+    private static final String userName = "root";
+    private static final String password = "669445";
+    private static final String dbUrl = "jdbc:mysql://localhost:3306/cse2062";
+    private Connection connection = null;
+    private static DatabaseManager instance;
+    private boolean isConnected = false;
+
+    public DatabaseManager() {
+        try {
+            connection = DriverManager.getConnection(dbUrl, userName, password);
+            if (connection.isValid(5)) {
+                isConnected = true;
+                System.out.println("Connected to database");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        instance = this;
+    }
+
+    public static DatabaseManager getInstance() {
+        return instance;
+    }
+
+    public Student checkUser(long id, String password) {
+        try {
+            PreparedStatement userStatement = connection.prepareStatement("SELECT * FROM users WHERE id = ? AND password = ?");
+            userStatement.setLong(1, id);
+            userStatement.setString(2, password);
+            ResultSet userResult = userStatement.executeQuery();
+            if (userResult.next()) {
+                boolean isStudent = userResult.getString("role").equalsIgnoreCase("student");
+                if (isStudent) {
+                    long studentID;
+                    studentID = userResult.getLong("id");
+                    PreparedStatement studentStatement = connection.prepareStatement("SELECT * FROM students WHERE id = ?");
+                    studentStatement.setLong(1, studentID);
+                    ResultSet studentResult = studentStatement.executeQuery();
+                    if (studentResult.next()) {
+                        return new Student(userResult.getString("name"), userResult.getString("surname"), userResult.getString("email"),
+                                userResult.getString("phone"), userResult.getString("address"), userResult.getString("password"),
+                                userResult.getBoolean("status"), userResult.getString("created_at"), userResult.getString("updated_at"),
+                                userResult.getString("role"), userResult.getLong("id"), studentResult.getFloat("gpa"),
+                                studentResult.getInt("totalCredits"), studentResult.getString("major"), studentResult.getString("expire_at"));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- src/com/jean/Main.java
+++ src/com/jean/Main.java
@@ -3,6 +3,12 @@ package com.jean;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        DatabaseManager manager = new DatabaseManager();
+        Student s1 = manager.checkUser(150720521, "111111");
+        if (s1.getStatus()) {
+            System.out.println("Welcome " + s1.getName());
+        } else {
+            System.out.println("Your account is not activated or expired!");
+        }
     }
 }
